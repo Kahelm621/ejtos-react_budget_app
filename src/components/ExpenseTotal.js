09ec123522a84f411d530978cea49f4dcfdfaba9
Kristi <123495259+Kahelm621@users.noreply.github.com>
2023-12-01 13:52:56 -0600

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total += item.cost);
-    }, 0);
+    // Accessing expenses and currency from the AppContext
+    const { expenses, currency } = useContext(AppContext);
+
+    // Calculating the total expenses using reduce
+    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+
+    // Formatting the total expenses with the currency
+    const formattedTotal = `${currency}${totalExpenses.toFixed(2)}`;
+
     return (
         <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+            {/* Displaying the total expenses with currency */}
+            <span>Spent so far: {formattedTotal}</span>
         </div>
     );
 };
+
 export default ExpenseTotal;
